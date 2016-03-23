9ff2c74527cdd7206d76c4dfaf5a5bc8da335861
Nathan <nathansherburn@gmail.com>
2016-03-23 12:28:54 +1100

    Make stages

---
+++ Stage 2/client/index.js
@@ -0,0 +1,30 @@
+var myApp = angular.module('myApp',[])
+
+myApp.service('HistoryService', function($http) {
+
+  var baseUrl = "http://localhost:8080/"
+
+  this.saveWord = function (newWord) {
+    var url = baseUrl + "saveCurrent"
+    return $http.post(url, {"word": newWord})
+  }
+})
+
+myApp.controller('MyController', function($scope, HistoryService) {
+  
+  // These $scope guys will be available in the HTML
+  $scope.newWord = 'cat'
+
+  $scope.saveThisWord = function () {
+    HistoryService.saveWord( $scope.newWord )
+    .then(saveSuccess, error)    
+  }
+
+  function saveSuccess (json) {
+    console.log(json)
+  }
+
+  function error (err) {
+    console.log(err)
+  }
+})
---
+++ Stage 3/client/index.js
@@ -0,0 +1,45 @@
+var myApp = angular.module('myApp',[])
+
+myApp.service('HistoryService', function($http) {
+
+  var baseUrl = "http://localhost:8080/"
+
+  this.saveWord = function (newWord) {
+    var url = baseUrl + "saveCurrent"
+    return $http.post(url, {"word": newWord})
+  }
+
+  this.getSaved = function () {
+    var url = baseUrl + "getSaved"
+    return $http.get(url)
+  }
+})
+
+myApp.controller('MyController', function($scope, HistoryService) {
+  
+  // These $scope guys will be available in the HTML
+  $scope.words = []
+  $scope.newWord = 'cat'
+
+  $scope.saveThisWord = function () {
+    HistoryService.saveWord( $scope.newWord )
+    .then(saveSuccess, error)    
+  }
+
+  $scope.getSavedWords = function() {
+    HistoryService.getSaved()
+    .then(loadSuccess, error)
+  }
+
+  function saveSuccess (json) {
+    // console.log(json)
+  }
+
+  function loadSuccess (json) {
+    $scope.words = json.data
+  }
+
+  function error (err) {
+    console.log(err)
+  }
+})
